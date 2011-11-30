7d04abbc63d546511a4ff04e58852ac69b50141e
Neil Essy <neilessy@gmail.com>
2011-11-30 02:56:34 -0800

    Added asynchronous io designed around actors

---
+++ src/main/scala/bluemold/actor/Persist.scala
@@ -0,0 +1,15 @@
+package bluemold.actor
+
+import java.io.{OutputStream, InputStream}
+import bluemold.io.{Persist => IOPersist}
+
+object Persist {
+  case class ReadBytes( read: Int, msg: Option[Any] )
+  case class WroteBytes( success: Boolean, msg: Option[Any] )
+  def read( data: Array[Byte], in: InputStream, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
+    IOPersist.read( data, in, ( read: Int ) => sender ! ReadBytes( read, replyWith ) )
+  }
+  def write( data: Array[Byte], out: OutputStream, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
+    IOPersist.write( data, out, ( success: Boolean ) => sender ! WroteBytes( success, replyWith ) )
+  }
+}
---
+++ src/main/scala/bluemold/io/Persist.scala
@@ -0,0 +1,74 @@
+package bluemold.io
+
+import java.util.concurrent._
+import java.io.{InputStream, IOException, OutputStream}
+
+
+object Persist {
+  val threadFactory = new ThreadFactory {
+    def newThread( r: Runnable ) = {
+      val thread = new Thread( r )
+      thread.setPriority( Thread.NORM_PRIORITY )
+      thread
+    }
+  }
+  val maxWorkerThreads = 32
+  val pool = new ThreadPoolExecutor(0, maxWorkerThreads, 60L, TimeUnit.SECONDS, new SynchronousQueue[Runnable], threadFactory)
+  
+  def write( data: Array[Byte], out: OutputStream, callback: (Boolean) => Unit ) {
+    out.synchronized {
+      data.synchronized {
+        pool.execute( Write( data, out, callback ) )
+      }
+    }
+  }
+  def read( data: Array[Byte], in: InputStream, callback: (Int) => Unit ) {
+    in.synchronized {
+      data.synchronized {
+        pool.execute( Read( data, in, callback ) )
+      }
+    }
+  }
+}
+
+case class Write( data: Array[Byte], out: OutputStream, callback: (Boolean) => Unit ) extends Runnable {
+  def run() {
+    try {
+      var done = false
+      out.synchronized {
+        data.synchronized {
+          try {
+            out.write( data )
+            out.flush()
+            done = true
+          } catch {
+            case t: IOException => // done is false
+          }
+        }
+      }
+      callback( done )
+    } catch {
+      case t: Throwable => t.printStackTrace()
+    }
+  }
+}
+
+case class Read( data: Array[Byte], in: InputStream, callback: (Int) => Unit ) extends Runnable {
+  def run() {
+    try {
+      var read = -1
+      in.synchronized {
+        data.synchronized {
+          try {
+            read = in.read( data )
+          } catch {
+            case t: IOException => // read is -1
+          }
+        }
+      }
+      callback( read )
+    } catch {
+      case t: Throwable => t.printStackTrace()
+    }
+  }
+}
