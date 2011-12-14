21e407cd40ec5b7e2dd762af8de1e012b2f4ddb1
Neil Essy <neilessy@gmail.com>
2011-12-14 02:45:29 -0800

    Added AsyncReader/Writer

--- src/main/scala/bluemold/actor/Persist.scala
+++ src/main/scala/bluemold/actor/Persist.scala
@@ -1,11 +1,15 @@
 package bluemold.actor
 
-import java.io.{OutputStream, InputStream}
-import bluemold.io.{Persist => IOPersist}
+import java.io.{File, OutputStream, InputStream}
+import bluemold.io.{AsyncReader, AsyncWriter, Persist => IOPersist}
 
 object Persist {
   case class ReadBytes( read: Int, msg: Option[Any] )
   case class WroteBytes( success: Boolean, msg: Option[Any] )
+
+  def getAsyncReader( file: File ) = IOPersist.getAsyncReader( file ) 
+  def getAsyncWriter( file: File ) = IOPersist.getAsyncWriter( file ) 
+  
   def read( data: Array[Byte], in: InputStream, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
     IOPersist.read( data, in, ( read: Int ) => sender ! ReadBytes( read, replyWith ) )
   }
@@ -14,5 +18,11 @@ object Persist {
   }
   def writeList( datas: List[Array[Byte]], out: OutputStream, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
     IOPersist.writeList( datas, out, ( success: Boolean ) => sender ! WroteBytes( success, replyWith ) )
+  }
+  def writeAsync( data: Array[Byte], offset: Long, out: AsyncWriter, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
+    IOPersist.writeAsync( data, offset, out, ( success: Boolean ) => sender ! WroteBytes( success, replyWith ) )
+  }
+  def readAsync( data: Array[Byte], offset: Long, in: AsyncReader, replyWith: Option[Any] = None )(implicit sender: ActorRef) {
+    IOPersist.readAsync( data, offset, in, ( read: Int ) => sender ! ReadBytes( read, replyWith ) )
   }
 }
--- src/main/scala/bluemold/io/Persist.scala
+++ src/main/scala/bluemold/io/Persist.scala
@@ -1,7 +1,8 @@
 package bluemold.io
 
 import java.util.concurrent._
-import java.io.{InputStream, IOException, OutputStream}
+import java.io._
+import java.nio.ByteBuffer
 
 
 object Persist {
@@ -15,27 +16,117 @@ object Persist {
   val maxWorkerThreads = 32
   val pool = new ThreadPoolExecutor(0, maxWorkerThreads, 60L, TimeUnit.SECONDS, new SynchronousQueue[Runnable], threadFactory)
   
+  def getAsyncReader( file: File ) = new AsyncReader( file ) 
+  def getAsyncWriter( file: File ) = new AsyncWriter( file ) 
+  
   def write( data: Array[Byte], out: OutputStream, callback: (Boolean) => Unit ) {
     out.synchronized {
       data.synchronized {
-        pool.execute( Write( data, out, callback ) )
+        try {
+          pool.execute( Write( data, out, callback ) )
+        } catch {
+          case e: RejectedExecutionException => callback( false )
+        }
       }
     }
   }
   def writeList( datas: List[Array[Byte]], out: OutputStream, callback: (Boolean) => Unit ) {
     out.synchronized {
       datas.synchronized {
-        pool.execute( WriteList( datas, out, callback ) )
+        try {
+          pool.execute( WriteList( datas, out, callback ) )
+        } catch {
+          case e: RejectedExecutionException => callback( false )
+        }
       }
     }
   }
   def read( data: Array[Byte], in: InputStream, callback: (Int) => Unit ) {
     in.synchronized {
       data.synchronized {
-        pool.execute( Read( data, in, callback ) )
+        try {
+          pool.execute( Read( data, in, callback ) )
+        } catch {
+          case e: RejectedExecutionException => callback( 0 )
+        }
       }
     }
   }
+
+  def writeAsync( data: Array[Byte], offset: Long, out: AsyncWriter, callback: (Boolean) => Unit ) {
+    data.synchronized {
+      try {
+        pool.execute( WriteToWriter( data, offset, out, callback ) )
+      } catch {
+        case e: RejectedExecutionException => callback( false )
+      }
+    }
+  }
+  def readAsync( data: Array[Byte], offset: Long, in: AsyncReader, callback: (Int) => Unit ) {
+    data.synchronized {
+      try {
+        pool.execute( ReadFromReader( data, offset, in, callback ) )
+      } catch {
+        case e: RejectedExecutionException => callback( 0 )
+      }
+    }
+  }
+}
+
+class AsyncReader( val file: File ) {
+  private[io] val randomAccess = new RandomAccessFile( file, "r" )
+  private[io] val channel = randomAccess.getChannel
+  private[io] def read( bytes: Array[Byte], offset: Long ): Int = {
+    val buffer = ByteBuffer.wrap( bytes )
+    val read = channel.read( buffer, offset )
+    if ( read == 0 ) {
+      synchronized { wait( 15 ) }
+      val read = channel.read( buffer, offset )
+      if ( read == 0 ) {
+        synchronized { wait( 15 ) }
+        channel.read( buffer, offset )
+      } else read
+    } else read
+  }
+  def close() { randomAccess.close() }
+}
+
+class AsyncWriter( val file: File ) {
+  private[io] val randomAccess = new RandomAccessFile( file, "rws" )
+  private[io] val channel = randomAccess.getChannel
+  private[io] def read( bytes: Array[Byte], offset: Long ): Int = {
+    val buffer = ByteBuffer.wrap( bytes )
+    val read = channel.read( buffer, offset )
+    if ( read == 0 ) {
+      synchronized { wait( 15 ) }
+      val read = channel.read( buffer, offset )
+      if ( read == 0 ) {
+        synchronized { wait( 15 ) }
+        channel.read( buffer, offset )
+      } else read
+    } else read
+  }
+  private[io] def write( bytes: Array[Byte], offset: Long ): Int = {
+    val buffer = ByteBuffer.wrap( bytes )
+    val written = channel.write( buffer, offset )
+    if ( written == 0 ) {
+      synchronized { wait( 15 ) }
+      val written = channel.write( buffer, offset )
+      if ( written == 0 ) {
+        synchronized { wait( 15 ) }
+        val written = channel.write( buffer, offset )
+        if ( written != 0 ) channel.force( true )
+        written
+      } else {
+        channel.force( true )
+        written
+      }
+    } else {
+      channel.force( true )
+      written
+    }
+  }
+  def close() { randomAccess.close() }
 }
 
 case class Write( data: Array[Byte], out: OutputStream, callback: (Boolean) => Unit ) extends Runnable {
@@ -49,7 +140,7 @@ case class Write( data: Array[Byte], out: OutputStream, callback: (Boolean) => U
             out.flush()
             done = true
           } catch {
-            case t: IOException => // done is false
+            case t: IOException => t.printStackTrace() // done is false
           }
         }
       }
@@ -65,13 +156,15 @@ case class WriteList( datas: List[Array[Byte]], out: OutputStream, callback: (Bo
     try {
       var done = false
       out.synchronized {
-        datas.synchronized {
-          try {
-            datas foreach { out.write( _ ) }
-            out.flush()
-            done = true
-          } catch {
-            case t: IOException => // done is false
+        datas foreach {
+          data => data.synchronized {
+            try {
+              out.write( data )
+              out.flush()
+              done = true
+            } catch {
+              case t: IOException => t.printStackTrace() // done is false
+            }
           }
         }
       }
@@ -91,8 +184,44 @@ case class Read( data: Array[Byte], in: InputStream, callback: (Int) => Unit ) e
           try {
             read = in.read( data )
           } catch {
-            case t: IOException => // read is -1
+            case t: IOException => t.printStackTrace() // read is -1
           }
+        }
+      }
+      callback( read )
+    } catch {
+      case t: Throwable => t.printStackTrace()
+    }
+  }
+}
+
+case class WriteToWriter( data: Array[Byte], offset: Long, out: AsyncWriter, callback: (Boolean) => Unit ) extends Runnable {
+  def run() {
+    try {
+      var done = false
+      data.synchronized {
+        try {
+          done = out.write( data, offset ) == data.length
+        } catch {
+          case t: IOException => t.printStackTrace() // done is false
+        }
+      }
+      callback( done )
+    } catch {
+      case t: Throwable => t.printStackTrace()
+    }
+  }
+}
+
+case class ReadFromReader( data: Array[Byte], offset: Long, in: AsyncReader, callback: (Int) => Unit ) extends Runnable {
+  def run() {
+    try {
+      var read = -1
+      data.synchronized {
+        try {
+          read = in.read( data, offset )
+        } catch {
+          case t: IOException => t.printStackTrace() // read is -1
         }
       }
       callback( read )
